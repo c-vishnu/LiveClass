a58671543cc976ea5b968e4b0fa1bfe78cb72d9f
c-vishnu <vishnu.chettyamveetil@neyyarapp.com>
2022-05-12 23:32:40 +0530

    commit-1

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
-import {Button, Space} from 'antd';
-import { ReactComponent as Chat} from '../icons/chat.svg';
-import { ReactComponent as Ask_Doubt} from '../icons/Ask_Doubt.svg';
-import { ReactComponent as Hand} from '../icons/hand.svg';
-import { ReactComponent as Settings} from '../icons/Setting.svg';
-import { ReactComponent as FullScreen} from '../icons/FullScreen.svg';
+import { Button, Space } from 'antd';
+import { ReactComponent as Chat } from '../icons/chat.svg';
+import { ReactComponent as Ask_Doubt } from '../icons/Ask_Doubt.svg';
+import { ReactComponent as Hand } from '../icons/hand.svg';
+import { ReactComponent as Settings } from '../icons/Setting.svg';
+import { ReactComponent as FullScreen } from '../icons/FullScreen.svg';
 
-const SideBar = () => {
-    return(
-        <div className="sideMenu">
-            <Space size={20}>
-            <Button type="link" icon={<Chat />} />
-            <Button type="link" icon={<Ask_Doubt />} />
-            <Button type="link" icon={<Hand />} />
-            <Button type="link" icon={<Settings />} />
-            <Button type="link" icon={<FullScreen />} />
-          </Space>
+const SideBar = (props) => {
+
+    const [toggleNav, setToggleNav] = useState(false);
+    const [buttonName, setButtonName] = useState();
+
+    const toggleClassName = () => {
+        setToggleNav(!toggleNav);
+    }
+
+    return (
+        <div className="side-nav-bar">
+            {props.setChild(toggleNav)}
+            <div className="nav-button-group">
+                <Button className="main-nav-button" type="link" icon={toggleNav?<FullScreen />:<Chat />} onClick={toggleClassName} />
+            </div>
+            <div className={`sideMenu ${toggleNav? "visible":"hidden"}`}>
+                <Space size={20} className="side-layout">
+                    <Button type="link" icon={<Chat />} />
+                    <Button type="link" icon={<Ask_Doubt />} />
+                    <Button type="link" icon={<Hand />} />
+                    <Button type="link" icon={<Settings />} />
+                    <Button type="link" icon={<FullScreen />} />
+                </Space>
+            </div>
         </div>
     )
 }
