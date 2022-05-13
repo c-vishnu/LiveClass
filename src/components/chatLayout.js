7b645afd8085ea8a26df58da842017961ceaec7e
c-vishnu <vishnu.chettyamveetil@neyyarapp.com>
2022-05-13 19:22:11 +0530

    commit-4

--- src/components/chatLayout.js
+++ src/components/chatLayout.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "../App.css";
+import { EllipsisOutlined } from '@ant-design/icons';
 
-function ChatLayout () {
-    return(
-        <div className="App">
-            <h4>Chat</h4>
+function ChatLayout() {
+    return (
+        <div className="Chat-body">
+            <div className="chatBubble-body">
+                <p className="chatBubble">
+                    <p>
+                        <div className="chat-bubble-button">
+                            <h4>Sender Name</h4>
+                            <EllipsisOutlined />
+                        </div>
+                        <p>Text Body of chat</p>
+                    </p>
+                </p>
+            </div>
         </div>
     )
 }
