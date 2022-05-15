286c66baa403fd2a84a25589485ecbb4692a5e32
c-vishnu <vishnu.chettyamveetil@neyyarapp.com>
2022-05-15 15:22:00 +0530

    css-1

--- src/components/chatLayout.js
+++ src/components/chatLayout.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "../App.css";
+import {Menu, Dropdown} from 'antd';
 import { EllipsisOutlined } from '@ant-design/icons';
 
 function ChatLayout() {
+
+    const menu =(
+        <Menu items={[
+          {
+            label: (
+                <div className="chat-option">
+                    <a >Warn<a className="warning"></a></a>
+                </div>
+            ),
+          },
+          {
+            label: (
+              <a>Block <a className="block"></a></a>
+            ), 
+          },
+        ]}/>
+      );
+
     return (
         <div className="Chat-body">
             <div className="chatBubble-body">
@@ -10,7 +29,9 @@ function ChatLayout() {
                     <p>
                         <div className="chat-bubble-button">
                             <h4>Sender Name</h4>
+                            <Dropdown overlay={menu} placement="top" arrow>
                             <EllipsisOutlined />
+                            </Dropdown>
                         </div>
                         <p>Text Body of chat</p>
                     </p>
