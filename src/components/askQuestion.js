aba22cdbcc39523bd5eb954c603f6376561d982f
c-vishnu <vishnu.chettyamveetil@neyyarapp.com>
2022-05-16 00:11:22 +0530

    commit-6

--- src/components/askQuestion.js
+++ src/components/askQuestion.js
@@ -3,9 +3,18 @@ import "../App.css";
 
 function AskQuestion ()  {
     return(
-        <div className="App">
-            <h4>Ask Questions</h4>
-        </div>
+            <div className="questionBubble-body">
+                <p className="que-bubble">
+                    <p>
+                        <h4>Name</h4>
+                        <p>Question body</p>
+                        <div className="que-bubble-button">
+                        <h4 style={{color:"green"}}>Answer</h4>
+                        <h4 style={{color:"red"}}>Reject</h4>
+                        </div>
+                    </p>
+                </p>
+            </div>
     )
 }
 
--- src/components/riseHand.js
+++ src/components/riseHand.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "../App.css";
+import {Switch} from 'antd';
 
 function RiseHand  () {
     return(
-        <div className="App">
-            <h4>Rise Hand</h4>
+        <div className="handRise-body">
+            <p className="riseHand-bubble">
+                <p>
+                    <div className="rh-bubble-body">
+                    <h4>Partcpt Name</h4>
+                    <p>Raised Hand</p>
+                    <p>time</p>
+                    </div>
+                    <div className="toggle-button-rh">
+                        <p>Start Audio</p>
+                        <Switch size="small" defaultChecked />
+                        <p>Start Video</p>
+                        <Switch size="small" defaultChecked />
+                    </div>
+                </p>
+
+            </p>
         </div>
     )
 }
