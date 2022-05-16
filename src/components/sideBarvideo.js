32cdb2960ed80ca51aaf7f450ce0a501f9526457
c-vishnu <vishnu.chettyamveetil@neyyarapp.com>
2022-05-16 10:36:36 +0530

    commit-7

---
+++ src/components/sideBarvideo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+const SidebarVideo =() =>{
+
+    return(
+        <div className="App">
+            <div className="videoLayout">
+                <video></video>
+            </div>
+        </div>
+    )
+}
+
+export default SidebarVideo;
