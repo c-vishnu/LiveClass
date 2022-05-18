d905287031809a2a732cd453ce9429551dfd5000
c-vishnu <vishnu.chettyamveetil@neyyarapp.com>
2022-05-18 11:06:23 +0530

    commit-12

--- src/components/askQuestion.js
+++ src/components/askQuestion.js
@@ -1,27 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import "../App.css";
 import {Input, Button} from 'antd';
+import axios from "./axios";
 import {  SendOutlined } from '@ant-design/icons';
 
 function AskQuestion ()  {
 
     const [answer, setAnswer] = useState(false);
+    const[questions, setQuestions]= useState([]);
+
+    useEffect(() => {
+        setInterval(()=> {
+            axios.get("/qanda")
+            .then((res) =>{
+                console.log(res);
+                setQuestions(res.data);
+            });
+        }, 10000);
+    },[]);
 
     const sendAnswer = () => {
         setAnswer(!answer);
     }
 
     return(
-            <div className="questionBubble-body">
+            <div className="questionBubble-body">{questions.map((data) => (
                 <p className="que-bubble">
                     <p style={{margin:"0px"}}>
-                        <h4>Name</h4>
-                        <p style={{margin:"0px"}}>Question body</p>
+                        <h4>{data.senderId}</h4>
+                        <p style={{margin:"0px"}}>{data.text}</p>
                         <div className="que-bubble-button">
                         <button onClick={sendAnswer}><h4 style={{color:"green"}}>Answer</h4></button>
                         <button><h4 style={{color:"red"}}>Reject</h4></button>
                         </div>
-                        <div>
+                    </p>
+                </p>
+           ))}
+                                   <div>
                         {
                             answer?
                             <div className="answer-body">
@@ -32,8 +47,6 @@ function AskQuestion ()  {
                         </div>:null
                         }
                         </div>
-                    </p>
-                </p>
             </div>
     )
 }
