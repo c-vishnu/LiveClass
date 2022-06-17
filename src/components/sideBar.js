c70c6071c49cea9c0846c38467c7839d8fe19dbe
Vishnu <cvishnuofficial@gmail.com>
2022-06-17 19:06:33 +0530

    ask-que-css-add

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -21,13 +21,14 @@ const SideBar = (props) => {
         <div className="side-nav-bar">
             {props.setChild(toggleNav)}
             <div className="nav-button-group">
-                <button className="main-nav-button" style={{backgroundColor:"Transparent"}} onClick={toggleClassName}>{toggleNav?<FullScreen />:<Chat />}</button>
+                <button className="main-nav-button" style={{backgroundColor:"Transparent"}} onClick={toggleClassName}><Chat /></button>
             </div>
             <div className={`sideMenu ${toggleNav? "visible":"hidden"}`}>
             <button onClick={()=>setButtonName('chat')}><Chat /></button>
             <button onClick={()=>setButtonName('askQuestions')}><AskDoubt /></button>
             <button onClick={()=>setButtonName('riseHand')}><Hand /></button>
             <button onClick={()=>setButtonName('settings')}><Settings /></button>
+            <button style={{paddingBottom: "5px"}} onClick={toggleClassName}><FullScreen /></button>
                 <SideNavLayout buttonName={buttonName} />
             </div>
         </div>
